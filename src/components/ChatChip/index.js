08cb4679c41f9f74674974e3d0b5a42fbc7cf153
Fandika Okdiba <mr.parker0@gmail.com>
2020-10-11 19:44:47 +0700

    add https, remove debug (#9)

--- src/components/ChatChip/index.js
+++ src/components/ChatChip/index.js
@@ -4,21 +4,18 @@ import { Avatar, Card } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { cssAvatar, cssCard, cssChatChip } from './style';
 
-const ChatChip = ({ isSender, text }) => {
-  console.log(text);
-  return (
-    <div className={cssChatChip({ isSender })}>
-      {!isSender && (
-        <div className={cssAvatar}>
-          <Avatar size={32} icon={<UserOutlined />} />
-        </div>
-      )}
-      <Card>
-        <div className={cssCard}>{text}</div>
-      </Card>
-    </div>
-  );
-};
+const ChatChip = ({ isSender, text }) => (
+  <div className={cssChatChip({ isSender })}>
+    {!isSender && (
+      <div className={cssAvatar}>
+        <Avatar size={32} icon={<UserOutlined />} />
+      </div>
+    )}
+    <Card>
+      <div className={cssCard}>{text}</div>
+    </Card>
+  </div>
+);
 
 ChatChip.propTypes = {
   isSender: bool,
