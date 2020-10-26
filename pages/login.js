635c90b5c42ffc37454e86e79c69c3d9a0653ca4
fandok <mr.parker0@gmail.com>
2020-10-27 00:22:41 +0700

    change chat page and add router

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { message, Form, Input, Button } from 'antd';
-import { cssLogin, cssLoginInput } from '../styles/login';
+
 import { useCookies } from 'react-cookie';
+import { useRouter } from 'next/router';
+
 import { postLogin } from '../src/fetcher.js';
+import { cssLogin, cssLoginInput } from '../styles/login';
+
 import 'antd/dist/antd.css';
 
 const Login = () => {
+  const router = useRouter();
   const [, setCookie] = useCookies(['login']);
   const [form] = Form.useForm();
 
-  const submitLogin = () => {
+  const submitLogin = e => {
+    e.preventDefault();
+
     const email = form.getFieldValue('email');
     const name = form.getFieldValue('name');
 
@@ -20,7 +27,7 @@ const Login = () => {
           message.success(msg, 3);
           setCookie('email', email, { path: '/' });
           setCookie('name', name, { path: '/' });
-          window.location.href('/');
+          router.push('/chat');
         } else {
           message.error(msg, 3);
           form.resetFields();
@@ -28,6 +35,11 @@ const Login = () => {
       })
       .catch(error => console.error(error));
   };
+
+  useEffect(() => {
+    // Prefetch the chat page
+    router.prefetch('/chat');
+  }, []);
 
   return (
     <Form className={cssLogin} form={form}>
